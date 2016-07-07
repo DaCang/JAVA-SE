1db6869a8a91ae383b34ef66c992b76adcf86b29
DaCang <1061300519@qq.com>
2016-07-07 22:34:05 +0800

    第二条：多个构造器参数

--- JAVA SE02/src/com/songyl/test/Test5.java
+++ JAVA SE02/src/com/songyl/test/Test5.java
@@ -2,21 +2,20 @@ package com.songyl.test;
 
 /**
  * @author Administrator
- * 第二条：多个构造器参数
+ * 绗簩鏉★細澶氫釜鏋勯�犲櫒鍙傛暟
  */
 public class Test5 {
 	public static void main(String[] args) {
 		UserInfo userInfo = new UserInfo(1, "song");
 		System.out.println(userInfo);
-		UserInfo userInfo1 = new UserInfo(1, null, 0, "甘肃");
+		UserInfo userInfo1 = new UserInfo(1, null, 0, "鐢樿們");
 		System.out.println(userInfo1);
-        //JavaBean模式		
+        //JavaBean妯″紡		
 		UserInfo userInfo2 = new UserInfo();
 		userInfo2.setUserId(2);
 		userInfo2.setUserName("syl");
 		userInfo2.setAge(25);
-		userInfo2.setNotes("甘肃");
+		userInfo2.setNotes("鐢樿們");
 		System.out.println(userInfo2);
-
 	}
 }
