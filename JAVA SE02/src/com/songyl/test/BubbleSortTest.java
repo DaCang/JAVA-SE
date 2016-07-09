ab5576730faf403e4686186ff0b728e1d50d8229
DaCang <1061300519@qq.com>
2016-07-10 00:18:49 +0800

    测试冒泡排序算法

---
+++ JAVA SE02/src/com/songyl/test/BubbleSortTest.java
@@ -0,0 +1,13 @@
+package com.songyl.test;
+
+import com.songyl.util.BubbleSort;
+
+/**
+ * @author Administrator
+ * 测试冒泡排序算法
+ */
+public class BubbleSortTest {
+	public static void main(String[] args) {
+		new BubbleSort(new Integer[] { 1, 2, 3, 4, 5 });
+	}
+}
