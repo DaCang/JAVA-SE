6a3e8798255ec39ec53870828d405c811efb3991
DaCang <1061300519@qq.com>
2016-08-18 11:16:04 +0800

    Update Demo.java

--- workLearning/Demo.java
+++ workLearning/Demo.java
@@ -1,5 +1,5 @@
 @Test
-public void test1() {//分解字符串
+public void test1() {//StringTokenizer分解字符串
 	String sql="insert into tbl_dept_job (dept_id,job_id,job_name,add_user,add_time,isflag)values('200801171302','specialty5','交换','system',to_char(sysdate,'YYYY-MM-DD HH24:MI:SS'),'0') " +
 			"◎insert into tbl_dept_job (dept_id,job_id,job_name,add_user,add_time,isflag)values('200801171302','specialty10','无线岗','system',to_char(sysdate,'YYYY-MM-DD HH24:MI:SS'),'0') " +
 			"◎insert into tbl_dept_job (dept_id,job_id,job_name,add_user,add_time,isflag)values('200801171302','specialty11','3G岗','system',to_char(sysdate,'YYYY-MM-DD HH24:MI:SS'),'0') ◎";
